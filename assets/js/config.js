877c3b10105d959ed11dce732b4b212328f2dfb2
saraswatacademy <saraswatpradeep89@gmail.com>
2026-06-22 22:43:10 +0530

    new

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,7 @@
 window.BASE_URL =
-    location.hostname === "127.0.0.1" ||
-    location.hostname === "localhost"
-        ? ""
-        : "/Saraswat_Academy";
+location.hostname === "localhost" ||
+location.hostname === "127.0.0.1"
+
+? ""
+
+: "/Saraswat_Academy_";
